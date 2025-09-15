4d97f969d5bb45370859a311be5b115378407f53
peter emeka <peteremeka51@gmail.com>
2025-09-15 05:49:29 +0100

    Added comment to every parts

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
+    // Question Two: Toggle the className of an Element
 function toggleClass(element, className) {
     element.classList.toggle(className);
 }
 
+
+    // Question Three: Toggle the className of Multiple Elements
 function toggleClass(element, className) {
     element.classList.toggle(className);
 }
